3eba6f2e353fe3724757f06c241e25a457d8c5f2
Moataz Ghazy <m.ghazy44@hotmail.com>
2023-12-15 11:46:50 -0500

    added style

--- public/js/home/scores.js
+++ public/js/home/scores.js
@@ -6,21 +6,21 @@ scoresSlider.innerHTML = `
 <div class="score-card">
   <div class="sport-info">
     <img src="imgs/${scores[0].sportIcon}">
-    <p>${scores[0].date}</p>
+    <h2>${scores[0].date}</h2>
     <p>${scores[0].sportName}</p>
   </div>
 
-  <div class="school1-info">
+  <div class="school-info">
     <img src="${scores[0].school1.img}">
     <p>${scores[0].school1.score}</p>
     <p>${scores[0].school1.name}</p>
   </div>
 
-  <div class="school2-info">
+  <div class="school-info">
     <img src="${scores[0].school2.img}">
     <p>${scores[0].school2.score}</p>
     <p>${scores[0].school2.name}</p>
   </div>
-
+<div></div>
 </div>
 `
