fa30ef1823aaee920286be9c5d5d1d56f71e5b33
Moataz Ghazy <m.ghazy44@hotmail.com>
2024-01-11 09:20:22 -0500

    catch events error

--- utils/events.js
+++ utils/events.js
@@ -6,16 +6,20 @@ async function getEvents() {
   const current = new Date(d).toISOString()
   const last = new Date(d + 604800000).toISOString()
 
-  const { key } = await Keys.findOne({_id: "659c0a5fd55feea809f1c460"})
-  const { data : { value }} = await axios.get(`https://graph.microsoft.com/v1.0/me/calendars/AAMkAGE4NzBkMTUzLTY2ZjEtNDA0ZS05NzZlLTZiMDI2Yjk0YWUxOABGAAAAAAAXlBkvp769TZGgsm_lxAtaBwDvMe1e-Kw_TJVDVSSeyrhQAAAAAAEHAAD2cOw24TbASJsIMgaYX8trAAVIVu9xAAA=/calendarView?StartDateTime=${current}&EndDateTime=${last}`, {
-    headers: {
-      "Authorization": `Bearer ${key}`,
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    }
-  })
-  const events = value.map(event => ({subject: event.subject, start: event.start.dateTime, end: event.end.dateTime}))
-  return events
+  try {
+    const { key } = await Keys.findOne({_id: "659c0a5fd55feea809f1c460"})
+    const { data : { value }} = await axios.get(`https://graph.microsoft.com/v1.0/me/calendars/AAMkAGE4NzBkMTUzLTY2ZjEtNDA0ZS05NzZlLTZiMDI2Yjk0YWUxOABGAAAAAAAXlBkvp769TZGgsm_lxAtaBwDvMe1e-Kw_TJVDVSSeyrhQAAAAAAEHAAD2cOw24TbASJsIMgaYX8trAAVIVu9xAAA=/calendarView?StartDateTime=${current}&EndDateTime=${last}`, {
+      headers: {
+        "Authorization": `Bearer ${key}`,
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+    const events = value.map(event => ({subject: event.subject, start: event.start.dateTime, end: event.end.dateTime}))
+    return events
+  } catch {
+    return []
+  }
 }
 
 module.exports = getEvents
